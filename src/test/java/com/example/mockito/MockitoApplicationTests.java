a10ff98ee88dfe8b1a09330f4cecef2eef06a9fe
taron <funkadelica@mail.ru>
2022-10-22 22:35:15 +0300

    firstCommit

---
+++ src/test/java/com/example/mockito/MockitoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+
+
+}
